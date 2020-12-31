3595f10fee56faf44a8f2c4e6ee3b69bc95a72a9
guodongggg <guodongggg@gmail.com>
2020-12-31 14:41:31 +0800

    优化格式

--- fund_base.py
+++ fund_base.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+
 def XiongAPI(url, code=None):
     params = {
         'code':code
@@ -15,15 +16,18 @@ def XiongAPI(url, code=None):
         raise Exception("API接口数据异常：" + response.text)
     else:
         return result
-        
+
+
 def BaseInfo(code):
     code = ','.join(code)
     url = 'https://api.doctorxiong.club/v1/fund'
     return XiongAPI(url,code).get('data',[])
 
+
 def stock_board():  
     url = 'https://api.doctorxiong.club/v1/stock/board'
     return XiongAPI(url).get('data', [])
+
 
 if __name__ == '__main__':
     with open('code_list.json','r') as f:
@@ -31,6 +35,8 @@ if __name__ == '__main__':
         code_list = json_data['test']    
     fundDetail = BaseInfo(code_list)
     board = stock_board()
-    print(board)
+    for i in board:
+        print(i)
     print('---')
-    print(fundDetail)
+    for i in fundDetail:
+        print(i)
--- update_over_json.py
+++ update_over_json.py
@@ -5,6 +5,7 @@ import json
 import fund_base
 from pathlib import Path
 
+
 def over_time(code_list):
     date_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
     date_today = datetime.datetime.now().strftime("%Y-%m-%d")
@@ -12,38 +13,44 @@ def over_time(code_list):
     # end_time = f"2020-12-31 12:00"
     end_time_stamp = time.mktime(time.strptime(end_time, '%Y-%m-%d %H:%M'))
     now_time_stamp = time.mktime(time.strptime(date_now, '%Y-%m-%d %H:%M'))
+
+    # 获取数据
+    board = fund_base.stock_board()
+    detail = fund_base.BaseInfo(code_list)
+    hs300 = ''
+    for i in board:
+        if i['code'] == 'sz399300':
+            hs300 = i['changePercent']
+
+    # 判断文件是否存在，不存在则创建
+    json_file_name = 'bj.json'
+    file = Path(json_file_name)
+    file.touch(exist_ok=True)
+
     if int(now_time_stamp) > int(end_time_stamp):
-        board = fund_base.stock_board()
-        detail = fund_base.BaseInfo(code_list)
-        hs300 = ''
-        for i in board:
-            if i['code'] == 'sz399300':
-                hs300 = i['changePercent']
         avg = average_growth.average_growth(detail)
-        file = Path('bj.json')
-        file.touch(exist_ok=True)
-        with open("bj.json", 'r+') as f:
-            try:
-                data = json.load(f)
-                f.seek(0, 0)
-                f.truncate()
-            except Exception as e:
-                print(e)
-                data = {}
-            finally:
-                print(f'get code_list.json data: {data}')
-                data[date_today] = {
-                    'HS300': hs300,
-                    'my_position': str(avg),
-                    'over_take': True if float(avg) > float(hs300) else False
-                }
-                print(f'update data: {data[date_today]}')
-                f.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
-                print('json update!')
-                return True
     else:
-        print(f"{date_now} 当前未休市，不更新数据")
-        return False
+        print(f"{date_now} 当前未休市，更新的仅为估值数据")
+        avg = average_growth.average_growth(detail, real=True)
+
+    with open("bj.json", 'r+') as f:
+        try:
+            data = json.load(f)
+            f.seek(0, 0)
+            f.truncate()
+        except Exception as e:
+            print(e)
+            data = {}
+        finally:
+            print(f'get {json_file_name} data: {data}')
+            data[date_today] = {
+                'HS300': hs300,
+                'my_position': str(avg),
+                'over_take': True if float(avg) > float(hs300) else False
+            }
+            print(f'update data: {data[date_today]}')
+            f.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+            print(f'{json_file_name} update!')
 
 
 if __name__ == '__main__':
