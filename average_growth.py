e02c4c68d75677baea41a439fda44e5584e89bba
guodongggg <guodongggg@gmail.com>
2021-01-04 12:32:27 +0800

    更新基金比例

--- average_growth.py
+++ average_growth.py
@@ -11,16 +11,16 @@ def average_growth(fund_data_list, real=False):
     # for i in fund_data_list:
     #     print(i['name'], i['expectGrowth'])
     fund_precentage = {
-        '005827': 0.1,
-        '163417': 0.11,
-        '001218': 0.29,
-        '001714': 0.03,
-        '162605': 0.07,
-        '001102': 0.06,
-        '161725': 0.02,
-        '002984': 0.02,
-        '004070': 0.07,
-        'nasdaq100': 0.23}
+        '005827': 0.12,  # 易方达蓝筹精选混合
+        '163417': 0.13,  # 兴全合宜混合(LOF)A
+        '001714': 0.03,  # 工银文体产业股票A
+        '162605': 0.12,  # 景顺长城鼎益混合LOF
+        '001102': 0.23,  # 前海开源国家比较优势混合
+        '161725': 0.02,  # 招商中证白酒指数分级
+        '002984': 0.03,  # 广发中证环保ETF联接C
+        '004070': 0.05,  # 南方全指证券联接C
+        'nasdaq100': 0.27  # 纳斯达克100
+        }
     if not real:
         list_ = [float(x['expectGrowth'])*fund_precentage[x['code']] for x in fund_data_list]
     else:
@@ -32,7 +32,7 @@ def average_growth(fund_data_list, real=False):
 
 
 if __name__ == '__main__':
-    code_list = ['005827','163417','001218','001714','162605','001102','161725','002984','004070']
+    code_list = ['005827','163417','001714','162605','001102','161725','002984','004070']
     detail = fund_base.BaseInfo(code_list)
     board = fund_base.stock_board()
     if not detail:
