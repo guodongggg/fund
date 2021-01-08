a2d137a6d1684e4d00fa2ff32fe3e10bc6671244
guodongggg <guodongggg@gmail.com>
2021-01-08 16:03:19 +0800

    1.增加基金组合和基准的比较图 2.增加基金组合的alpha beta sharpe值 3.增加最大回撤

--- run.py
+++ run.py
@@ -89,12 +89,24 @@ def xalpha_all():
     v_position_all = sysopen.v_category_positions()  # 仓位饼状图（大类）
     v_position_history = sysopen.v_positions_history()  # 仓位水流图
     all_trade = sysopen.v_tradevolume(freq='W')  # 整体交易坐标图
+    sysclose = xa.mulfix(*sysopen.fundtradeobj, totmoney=40000)
+    sysclose.bcmkset(xa.indexinfo('1399300'))  # 设置比较基准，开始对此封闭的基金投资系统进行量化评估 首位0
+    close_netvalue = sysclose.v_netvalue()  # 基金组合与基准比较收益图
+    max_drawdown = sysclose.max_drawdown()  # 最大回撤
+    alpha = sysclose.alpha()
+    beta = sysclose.beta()
+    sharpe = sysclose.sharpe()
     context = {
         'annualized_rate': annualized_rate,
         'v_position': v_position,
         'v_position_all': v_position_all,
         'v_position_history': v_position_history,
-        'all_trade': all_trade
+        'all_trade': all_trade,
+        'close_netvalue': close_netvalue,
+        'max_drawdown': max_drawdown,
+        'alpha': alpha,
+        'beta': beta,
+        'sharpe': sharpe
     }
     return render_template('xalpha_all.html', **context)
 
