5c42c105269b0c0eb2476e9bb9e44d47b8ac3cd9
guodongggg <guodongggg@gmail.com>
2021-01-15 17:11:54 +0800

    使用JQuery语法替代JS

--- static/redorgreen.js
+++ static/redorgreen.js
@@ -1,19 +1,36 @@
 function redorgreen(){
-    var num = document.getElementsByClassName('num');
-    var list = Array.from(num);
-    list.map(function(item){
-        value = item.textContent.replace('%','');
+    var num = $('.num');
+    num.map(function(){
+        var value = this.textContent.replace('%','');;
         if (value){
             if (value > 0){
-                item.style.color = 'red';
+                this.style.color = 'red';
             }
             else {
-                item.style.color = 'green';
+                this.style.color = 'green';
             }
         }
         else{
-            item.innerText = '无估值';
-            item.style.color = '#666';
+            this.innerText = '无估值';
+            this.style.color = '#666';
         }
-    })}
+    })
+}
+//    var num = document.getElementsByClassName('num');
+//    var list = Array.from(num);
+//    list.map(function(item){
+//        value = item.textContent.replace('%','');
+//        if (value){
+//            if (value > 0){
+//                item.style.color = 'red';
+//            }
+//            else {
+//                item.style.color = 'green';
+//            }
+//        }
+//        else{
+//            item.innerText = '无估值';
+//            item.style.color = '#666';
+//        }
+//    })}
 redorgreen();
