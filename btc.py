edf749d4987bc0b817061565f1c5cf2844731f80
Jelly <guodongggg@gmail.com>
2021-03-01 15:20:17 +0800

    修复btc获取失败的问题 banner增加集思录

--- btc.py
+++ btc.py
@@ -2,20 +2,31 @@ import requests
 
 
 def btc():
-    url = 'https://www.usd-cny.com/btc/b.js'
-    r = requests.get(url).text
-    btc_value = r.split(',')[8]
-    return int(btc_value[:5])
+    def usd_cny():
+        try:
+            url = 'https://www.usd-cny.com/btc/b.js'
+            r = requests.get(url).text
+            btc_value = r.split(',')[8]
+            return int(btc_value[:5])
+        except:
+            raise Exception('usd_cny获取btc失败')
 
-
-def btcfans():
-    from lxml import etree
-    url = 'https://price.btcfans.com/coin/bitcoin'
-    r = requests.get(url).text
-    html = etree.HTML(r)
-    btc = html.xpath('/html/body/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/div[1]/text()')[0].strip()[1:]
-    return btc
+    def btcfans():
+        try:
+            from lxml import etree
+            url = 'https://price.btcfans.com/coin/bitcoin'
+            r = requests.get(url).text
+            html = etree.HTML(r)
+            btc = html.xpath('/html/body/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/div[1]/text()')[0].strip()[1:]
+            btc = btc.split('.')[0].replace(',', '')
+            return btc
+        except:
+            raise Exception('btcfans获取btc失败')
+    try:
+        return usd_cny()
+    except:
+        return btcfans()
 
 
 if __name__ == '__main__':
-    print('BTC:', btcfans())
+    print('BTC:', btc())
