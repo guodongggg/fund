ab150c5f28d57607c1b546ba38c6f9d40808202c
Jelly <guodongggg@gmail.com>
2021-05-14 10:47:20 +0800

    优化btc查询接口

--- btc.py
+++ btc.py
@@ -1,36 +1,25 @@
 import requests
 
 
-def btc():
+def btcfans(coinname):
     """
     获取btc当前的市值
     :return: str
     """
-    def usd_cny():
-        try:
-            url = 'https://www.usd-cny.com/btc/b.js'
-            r = requests.get(url).text
-            btc_value = r.split(',')[8]
-            return int(btc_value[:5])
-        except:
-            raise Exception('usd_cny获取btc失败')
-
-    def btcfans():
-        try:
-            from lxml import etree
-            url = 'https://price.btcfans.com/coin/bitcoin'
-            r = requests.get(url).text
-            html = etree.HTML(r)
-            btc = html.xpath('/html/body/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/div[1]/text()')[0].strip()[1:]
-            btc = btc.split('.')[0].replace(',', '')
-            return btc
-        except:
-            raise Exception('btcfans获取btc失败')
     try:
-        return usd_cny()
+        from lxml import etree
+        url = f'https://price.btcfans.com/coin/{coinname}'
+        r = requests.get(url).text
+        html = etree.HTML(r)
+        price = html.xpath('/html/body/div[1]/div[3]/div/div[1]/div[2]/div[1]/div[1]/div[1]/span[2]/text()')[0]
+        growth = html.xpath('/html/body/div[1]/div[3]/div/div[1]/div[2]/div[1]/div[2]/div[2]/text()')[0]
+        return {'price': price, 'growth': growth}
     except:
-        return btcfans()
+        raise Exception(f'btcfans获取{coinname}失败')
 
 
 if __name__ == '__main__':
-    print('BTC:', btc())
+    coinname_list = {'bitcoin', 'ethereum', 'shiba'}
+    for i in coinname_list:
+        coin = btcfans(i)
+        print(i, coin['price'], coin['growth'])
