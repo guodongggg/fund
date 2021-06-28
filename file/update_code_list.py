52cd48dab4df5a3736cee50ac34475c636e3efed
Jelly <guodongggg@gmail.com>
2021-06-28 11:25:35 +0800

    更新持仓 修改基金排序

--- file/update_code_list.py
+++ file/update_code_list.py
@@ -9,8 +9,9 @@ with open('code_list.json', 'r+', encoding='UTF-8') as f:
     print(f'总持仓:￥{count}')
     for key, value in prd.items():
         value['percent'] = float("{:.2f}".format(value['count']/count))
+    data['product'] = dict(sorted(prd.items(), key=lambda x: x[1]['count'], reverse=True))
     f.seek(0, 0)
     f.truncate()
-    f.write(json.dumps(data, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ': ')))
+    f.write(json.dumps(data, ensure_ascii=False, indent=4, separators=(',', ': ')))
 
 print('更新完成！')
